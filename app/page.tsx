a60392853bb9f36e317f7da42e5d0f67853cd4fd
Niteshgairola01 <niteshgairolapapl@gmail.com>
2026-01-07 12:37:43 +0530

    Create Navbar component and setup clerk for authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <div></div>
+    <div className="min-h-screen bg-linear-to-br from-blue-100 via-white to-purple-100">
+      <Navbar />
+    </div>
   );
 }
